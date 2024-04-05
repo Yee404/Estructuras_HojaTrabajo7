64649844f4f0a2f43d67d92508d9626bfa275d4e
Yee404 <yee231193@uvg.edu.gt>
2024-04-04 23:55:16 -0600

    Commit2
    Terminar de subir los recursos del BinarySearchTree para el programa

---
+++ Association.java
@@ -0,0 +1,23 @@
+public class Association {
+    private String ingles;
+    private String espannol;
+    private String frances;
+
+    public Association(String ingles, String espannol, String frances) {
+        this.ingles = ingles;
+        this.espannol = espannol;
+        this.frances = frances;
+    }
+
+    public String getIngles() {
+        return ingles;
+    }
+
+    public String getEspannol() {
+        return espannol;
+    }
+
+    public String getFrancia() {
+        return frances;
+    }
+}
